393dec66ee163b46f301c7257bddbf6e8a902b84
Maksim Safin <crockolaco@yandex.ru>
2020-08-26 09:33:20 +0500

    remove

--- src/lib/cardsEqual.js
+++
@@ -1,7 +0,0 @@
-import {shallowEqual} from "./shalowEqual";
-
-export const cardsEqual = (first, second) => {
-    const equal = shallowEqual(first, second);
-    if (first.id === second.id) return false;
-    return equal;
-}
