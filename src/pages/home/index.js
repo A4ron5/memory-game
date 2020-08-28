733015d98df2f5fc4b3bc68097eabb7bbdae5b82
Maksim Safin <crockolaco@yandex.ru>
2020-08-28 09:14:42 +0500

    pages(home): add features

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,15 @@
 import React from "react";
+
 import {CardList} from "../../features/card-list";
+import {Counter} from "../../features/counter";
+import {RestartButton} from "../../features/restart-button";
 
 export const Home = () => {
 
   return (
     <main>
+        <RestartButton/>
+        <Counter/>
         <CardList/>
     </main>
   );
