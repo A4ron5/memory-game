8cbdd72249885604076bb19c8b20b9bb038eee73
Maksim Safin <crockolaco@yandex.ru>
2020-08-28 09:15:43 +0500

    features(card-list) move logic to comparator

--- src/features/card-list/model.js
+++ src/features/card-list/model.js
@@ -1,48 +1,19 @@
-import {createEvent, createStore, sample, guard, split} from "effector";
+import {createStore, sample} from "effector";
 
-import {$selectedCards, selectedCardsCleared, cardSelected} from "../card/model";
+import {cardSelected} from "../card/model";
+import {comparedCardsFailed, comparedCardsSuccess} from "../comparator/model";
+
 import {createDeck} from "../../api";
-import {compareCards} from "../../lib/compareCards";
-import {getMarkedCards} from '../../lib/getMarkedCards';
+import {getMarkedCards} from "../../lib/getMarkedCards";
 
 export const $cards = createStore(createDeck());
-export const $isCardPair = $selectedCards.map(cards => cards.length === 2);
-export const $isGameOver = $cards.map(card => !card.open)
 
-export const recreatedDeck = createEvent();
-const comparedCards = createEvent();
-const comparedCardsFinished = createEvent();
-const comparedCardsSuccess = createEvent();
-const comparedCardsFailed = createEvent();
-const cardSelectedSampled = sample($cards, cardSelected, getMarkedCards)
+const cardMarked = sample($cards, cardSelected, getMarkedCards)
 
 $cards
-    .on(recreatedDeck, () => createDeck())
-    .on(cardSelectedSampled, (_, payload) => payload);
-
-guard({
-    source: $selectedCards,
-    filter: $isCardPair,
-    target: comparedCards
-})
-
-sample({
-    source: $selectedCards,
-    clock: comparedCards,
-    fn: source => compareCards(...source),
-    target: comparedCardsFinished
-})
-
-guard({
-    source: comparedCardsFinished,
-    filter: value => value,
-    target: [comparedCardsSuccess, selectedCardsCleared]
-})
-
-guard({
-    source: comparedCardsFinished,
-    filter: value => !value,
-    target: [selectedCardsCleared]
-})
+    .on(cardMarked, (_, payload) => payload)
+    .on(comparedCardsSuccess, () => {})
+    .on(comparedCardsFailed, () => {})
+;
 
 $cards.watch(console.log)
