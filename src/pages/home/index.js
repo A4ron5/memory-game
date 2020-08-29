6f0876ea036c0bb1e2000e5350ce879c1624e948
Maksim Safin <crockolaco@yandex.ru>
2020-08-29 15:13:16 +0500

    pages(home): update styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+import {Layout} from "../../ui/layout";
 
 import {CardList} from "../../features/card-list";
 import {Counter} from "../../features/counter";
 import {RestartButton} from "../../features/restart-button";
 
+const Container = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`
+
 export const Home = () => {
 
   return (
-    <main>
+    <Layout>
         <RestartButton/>
         <Counter/>
-        <CardList/>
-    </main>
+        <Container>
+            <CardList/>
+        </Container>
+    </Layout>
   );
 };
