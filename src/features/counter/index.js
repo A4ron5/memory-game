2fc9db9656451472d911d95a01a6de7c502b768d
Maksim Safin <crockolaco@yandex.ru>
2020-08-29 15:13:31 +0500

    features(counter): update styles

--- src/features/counter/index.js
+++ src/features/counter/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {useStore} from "effector-react";
 
 import {$counter} from "./model";
+import {StyledCounter} from "./ui";
 
 export const Counter = () => {
     const counter = useStore($counter);
+    const score = `Score \n${counter}`
 
     return (
-        <div>{counter}</div>
+        <StyledCounter>
+            {score}
+        </StyledCounter>
     )
 }
---
+++ src/features/counter/ui/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledCounter = styled.div`
+    text-align: center;
+    margin-bottom: 2rem;
+    font-size: 1.125rem;
+    color: #fff;
+    white-space: pre-wrap;
+`;
