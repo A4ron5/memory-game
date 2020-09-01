d16836a97842bb102b2a509778b36e1722c780a2
Maksim Safin <crockolaco@yandex.ru>
2020-09-01 09:15:06 +0500

    features(logotype/ui/molecules): add

---
+++ src/features/logotype/ui/molecules/Logotype.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import logo from '../../../../assets/logo.png'
+import {Img} from "../atoms";
+
+export const StyledLogotype = () => {
+    return <Img src={logo} alt='Memory Game'/>
+}
