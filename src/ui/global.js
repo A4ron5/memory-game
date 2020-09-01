93b6aed9e65f9de36c41277b8abe64c901b34ed7
Maksim Safin <crockolaco@yandex.ru>
2020-09-01 09:15:26 +0500

    ui(global): add background

--- src/ui/global.js
+++ src/ui/global.js
@@ -1,13 +1,16 @@
 import {createGlobalStyle} from 'styled-components';
 import {normalize} from "styled-normalize";
 
+import background from '../assets/background.png'
+
 export const GlobalStyles = createGlobalStyle`
   ${normalize};
   
   body {
     margin: 0;
-    background: mediumseagreen;
     font-size: 14px;
     box-sizing: border-box;
+    background-color: green;
+    background-image: url(${background});
   }
 `
