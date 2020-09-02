24a06418cce19735897db39cdc25246e8fb71703
Maksim Safin <crockolaco@yandex.ru>
2020-09-02 09:43:13 +0500

    pages(end): add

---
+++ src/pages/end/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
+import {Counter} from "../../features/counter";
+import {Button} from "../../ui/atoms";
+
+const Container = styled.div`
+  color: #fff;
+  margin: 300px auto 0;
+`
+
+export const EndPage = () => {
+    return (
+        <Container>
+            <Counter secondary/>
+            <Button>
+                <Link to="/game">Try again</Link>
+            </Button>
+        </Container>
+    )
+}
