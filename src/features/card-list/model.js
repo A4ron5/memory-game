bc53acabaddb69c2546dc17647f07d220f0dc333
Maksim Safin <crockolaco@yandex.ru>
2020-09-03 09:10:17 +0500

    features(card-list): add comparedSuccess

--- src/features/card-list/model.js
+++ src/features/card-list/model.js
@@ -1,20 +1,22 @@
 import {createStore, sample} from "effector";
 
 import {cardSelected} from "../card/model";
-import {comparedCardsFailed} from "../comparator/model";
+import {comparedCardsFailed, comparedCardsSuccess} from "../comparator/model";
 
 import {history} from "../../lib/routing";
 import {createDeck} from "../../api";
-import {getMarkedCard, getUnmarkedCards} from "../../lib/getCards";
+import {getMarkedCard, getComparedFailed, getComparedSuccess} from "../../lib/getCards";
 
 export const $cards = createStore(createDeck());
 
 const cardMarked = sample($cards, cardSelected, getMarkedCard)
-const cardsUnmarked = sample($cards, comparedCardsFailed, getUnmarkedCards);
+const comparedFailed = sample($cards, comparedCardsFailed, getComparedFailed);
+const comparedSucccess = sample($cards, comparedCardsSuccess, getComparedSuccess)
 
 $cards
     .on(cardMarked, (_, payload) => payload)
-    .on(cardsUnmarked, (_, payload) => payload)
+    .on(comparedFailed, (_, payload) => payload)
+    .on(comparedSucccess, (_, payload) => payload)
 ;
 $cards.watch(cards => {
     const isGameOver = cards.every(card => card.open === true)
@@ -23,3 +25,5 @@ $cards.watch(cards => {
         history.push('/end')
     }
 })
+
+$cards.watch(console.log)
