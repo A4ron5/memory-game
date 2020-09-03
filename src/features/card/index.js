400fa5fc71950ac15aab61fd898d09ce553f1e51
Maksim Safin <crockolaco@yandex.ru>
2020-09-03 09:09:59 +0500

    features(card): add disable

--- src/features/card/index.js
+++ src/features/card/index.js
@@ -3,16 +3,17 @@ import React from 'react';
 import {cardClicked} from "./model";
 import {StyledCard} from "./ui";
 
-export const Card = ({rank, suit, id, open}) => {
+export const Card = ({rank, suit, id, open, disable}) => {
 
     const clickHandler = () => {
-        if (open) return;
-        cardClicked({rank, suit, id, open})
+        if (open || disable) return;
+        cardClicked({rank, suit, id, open, disable})
     }
 
     return (
         <StyledCard
            open={open}
+           disable={disable}
            rank={rank}
            suit={suit}
            clickHandler={clickHandler}
