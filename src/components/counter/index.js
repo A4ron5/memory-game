5452fe416b3a9e3ff5d4437f963288be9e8fc24f
Maksim Safin <crockolaco@yandex.ru>
2020-09-05 20:34:49 +0500

    components(counter): replace model path

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {useStore} from "effector-react";
 
-import {$counter} from "./model";
+import {$score} from "../../models/score";
+
 import {StyledCounter} from "./ui";
 
 export const Counter = ({secondary}) => {
-    const counter = useStore($counter);
+    const counter = useStore($score);
     const score = `Score \n${counter}`
 
     return (
