5d0a7cc91e3bf67b35a8572bee38cc33e3a17486
Maksim Safin <crockolaco@yandex.ru>
2020-09-05 20:35:20 +0500

    remove old models

--- src/components/restart-button/index.js
+++ src/components/restart-button/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import {restart} from "./model";
 
 export const RestartButton = () => {
-    return <button onClick={restart}>Restart</button>
+    return <button onClick={() => {console.log('restart')}}>Restart</button>
 }
