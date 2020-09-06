8ee288738c11f6c65fc674c6f43db651a2ee9fb9
Maksim Safin <crockolaco@yandex.ru>
2020-09-06 12:43:00 +0500

    models(cards): init compared finished effect

--- src/models/cards/index.js
+++ src/models/cards/index.js
@@ -4,24 +4,28 @@ import {createDeck} from "../../api";
 export const cardClicked = createEvent();
 export const cardSelected = createEvent();
 export const cardCompared = createEvent();
-export const cardComparingFinished = createEvent();
+export const cardComparingFinishedFx = createEffect({
+    handler: res => new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const {status, cards} = res;
 
-export const cardComparingSuccessFx = createEffect({
-    handler: res => new Promise((resolve) => {
-        setTimeout(() => resolve(res), 1500)
+            if (status === 'success') {
+                resolve(cards)
+            } else {
+                reject(cards)
+            }
+        }, 1000)
     })
-})
-export const cardComparingFailedFx = createEffect({
-    handler: res => new Promise((_, reject) => {
-        setTimeout(() => reject(res), 1500)
-    })
-})
+});
+
 
 export const $cards = createStore(createDeck());
 export const $comparingCards = createStore([]);
 
 export const $isCardsLessThanTwo = $comparingCards.map(cards => cards.length < 2)
 export const $isPairOfCards = $comparingCards.map(cards => cards.length === 2);
+export const $isGameOver = $cards.map(cards => cards.every(card => card.disable === true));
 
 $comparingCards.watch(console.log)
 $isPairOfCards.watch(console.log)
+$isGameOver.watch(console.log)
