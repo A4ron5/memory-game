3e2779f2fce6cc579ca62a7f7d3530b356b49b9d
Maksim Safin <crockolaco@yandex.ru>
2020-09-06 12:45:45 +0500

    remove old models

--- src/components/counter/model.js
+++
@@ -1,8 +0,0 @@
-import {createStore} from "effector";
-import {comparedCardsSuccess, comparedCardsFailed} from '../comparator/model';
-
-export const $counter = createStore(0);
-
-$counter
-    .on(comparedCardsSuccess, (state) => state + 70)
-    .on(comparedCardsFailed, (state) => state - 35);
