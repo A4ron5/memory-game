d33704c51f4b8b084813b8d8a76bef186bb8268c
Maksim Safin <crockolaco@yandex.ru>
2020-09-10 11:51:54 +0500

    models(cards): moved effect handler

--- src/models/cards/index.js
+++ src/models/cards/index.js
@@ -1,24 +1,13 @@
 import {createEvent, createEffect, createStore} from "effector";
+import {createGate} from "effector-react";
 import {createDeck} from "../../api";
 
+export const initedCards = createEffect();
 export const cardClicked = createEvent();
 export const cardSelected = createEvent();
 export const cardCompared = createEvent();
 
-export const cardComparingFinishedFx = createEffect({
-    handler: res => new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const {status, cards} = res;
-
-            if (status === 'success') {
-                resolve(cards)
-            } else {
-                reject(cards)
-            }
-        }, 1000)
-    })
-});
-
+export const cardComparingFinishedFx = createEffect();
 
 export const $cards = createStore(createDeck());
 export const $comparingCards = createStore([]);
@@ -27,6 +16,6 @@ export const $isCardsLessThanTwo = $comparingCards.map(cards => cards.length < 2
 export const $isPairOfCards = $comparingCards.map(cards => cards.length === 2);
 export const $isGameOver = $cards.map(cards => cards.every(card => card.disable === true));
 
-$comparingCards.watch(console.log)
-$isPairOfCards.watch(console.log)
-$isGameOver.watch(console.log)
+export const GamePageGate = createGate();
+
+$cards.watch(console.log)
